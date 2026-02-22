3cdf901a3920fe547f7115f3e6fbc6007288f98e
Krowten <krowtenmobile@yahoo.com>
2026-02-22 08:49:47 -0500

    Added Rewards to Quest Tab Added Footer Added Find Spells button Created Functionality for Find Spells Button Populated Spell.dbc list Added Find Title Button Added functionality to Find Title Button Populated Titles list from CharTitles.dbc Added Changelog Splash screen QOL Changes to Find Buttons

---
+++ AzerothCoreCreator/ChangelogWindow.xaml.cs
@@ -0,0 +1,44 @@
+using System.Windows;
+
+namespace AzerothCoreCreator
+{
+    public partial class ChangelogWindow : Window
+    {
+        public string TitleText { get; set; }
+        public string ChangelogText { get; set; }
+
+        public ChangelogWindow(string currentVersion)
+        {
+            InitializeComponent();
+
+            TitleText = $"AzerothCore Creator v{currentVersion} - Changelog";
+
+            // Update this text when you release. Newest entries at top.
+            ChangelogText =
+@"v0.1.7
+- Rewards section added to Quest tab
+- Find Spell popout (offline list)
+- Find Title popout (offline list)
+- XP Reward dropdown options
+- Money reward coin UI
+- Footer copyright restored
+- QoL Updates to Find Buttons
+
+v0.1.6
+- Quest tab cleanup
+- Creature tab beginner-friendly flags
+- Item preview fixes
+
+v0.1.0
+- Initial beta release
+";
+
+            DataContext = this;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
